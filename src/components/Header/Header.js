528881ced9d5ccb68c61f62419d9862d3a0a8b7e
Rahlus Karl Morsi <github@rahlus.com>
2022-01-01 11:32:32 -0600

    Exercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -13,14 +13,7 @@ const Header = () => {
     <header>
       <SuperHeader>
         <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
+          <MenuButtonGroup />
           <ActionGroup>
             <button>
               <User size={24} />
@@ -29,9 +22,37 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopOnly>
+          <MenuButtonGroup />
+        </DesktopOnly>
         <Logo />
+        <DesktopOnly>
+          <SubscribeButtonGroup />
+        </DesktopOnly>
       </MainHeader>
     </header>
+  );
+};
+
+const MenuButtonGroup = () => {
+  return (
+    <ActionGroup>
+      <button>
+        <Search size={24} />
+      </button>
+      <button>
+        <Menu size={24} />
+      </button>
+    </ActionGroup>
+  );
+};
+
+const SubscribeButtonGroup = () => {
+  return (
+    <SubscribeDiv>
+      <Button>Subscribe</Button>
+      <SubscribeLink href="#no-where">Already a subscriber?</SubscribeLink>
+    </SubscribeDiv>
   );
 };
 
@@ -39,6 +60,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +90,29 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+  }
+`;
+
+const DesktopOnly = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`;
+
+const SubscribeDiv = styled.div`
+  text-align: center;
+`;
+
+const SubscribeLink = styled.a`
+  font-weight: ${WEIGHTS.normal};
+  font-style: italic;
+  font-size: calc(1rem * 14 / 16);
+  text-decoration: underline;
 `;
 
 export default Header;
